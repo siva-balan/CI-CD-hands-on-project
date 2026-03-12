37305aa2b62960af3b90b68779c303c9e3a9b24d
Sivabalan <sivabalan.arun@gmail.com>
2026-03-12 17:43:56 +0530

    updated deploy and added dockerfile.agent

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-container=website
-image=website
+container=fix_log_analyzer
+image=fix_log_analyzer
 
 echo "Stopping container: $container"
 docker stop $container 2>/dev/null
 docker rm $container 2>/dev/null
 
 echo "Staring new  container $container with image $image"
-docker run -d -p 8090:80 --name $container $image
+docker run -d -p 8090:5000 --name $container $image
